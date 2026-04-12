51487d3227d7db95b79dcdc7c32293c4f5fcc4f1
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-04-12 17:35:27 +0530

    fix: linting

--- backend/src/configs/queueConfig.js
+++ backend/src/configs/queueConfig.js
@@ -1,8 +1,8 @@
-import { Queue } from "bullmq";
+import { Queue } from 'bullmq';
 
-export const emailQueue = new Queue("emailQueue", {
-    connection: {
-        host: "127.0.0.1",
-        port: 6379,
-    },
+export const emailQueue = new Queue('emailQueue', {
+  connection: {
+    host: '127.0.0.1',
+    port: 6379
+  }
 });

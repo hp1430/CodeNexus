842c934e22a3d9d75b0d0a18c2b0504dc6d4daa2
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-04-12 18:29:12 +0530

    feat: added otp repository

---
+++ backend/src/repositories/otpRepository.js
@@ -0,0 +1,43 @@
+import Otp from '../schema/otp.js';
+
+const otpRepository = {
+  upsertOtp: async ({ userId, otpHash, expiresAt }) => {
+    const otpDoc = await Otp.findOneAndUpdate(
+      { userId },
+      {
+        otpHash,
+        expiresAt,
+        attempts: 0
+      },
+      {
+        upserts: true,
+        new: true
+      }
+    );
+    return otpDoc;
+  },
+
+  // find otp by userId
+  findOtpByUserId: async (userId) => {
+    const otpDoc = await Otp.findOne({ userId });
+    return otpDoc;
+  },
+
+  // delete otp after successfull verification
+  deleteOtpByUserId: async (userId) => {
+    const result = await Otp.deleteOne({ userId });
+    return result;
+  },
+
+  // increment attmepts
+  incrementAttempts: async (userId) => {
+    const updatedOtp = await Otp.findOneAndUpdate(
+      { userId },
+      { $inc: { attempts: 1 } },
+      { new: true }
+    );
+    return updatedOtp;
+  }
+};
+
+export default otpRepository;

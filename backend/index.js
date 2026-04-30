56af47ce18aa948dc53792941fb63440af52231b
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-04-30 16:21:17 +0530

    feat: added socket.io config

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import cors from 'cors';
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
 import connectDB from './src/configs/dbConfig.js';
 import { PORT } from './src/configs/serverConfig.js';
@@ -17,7 +19,28 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('joinRoom', (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });

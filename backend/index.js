ac4d2fe89f0395cebb0945f1b185310bc8916a40
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-05-01 16:29:27 +0530

    fix: added persitence for late joining users

--- backend/index.js
+++ backend/index.js
@@ -28,10 +28,12 @@ const io = new Server(server, {
   }
 });
 
+const rooms = {}; // In-memory store for rooms and participants
+
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  playgroundEventHandler(io, socket);
+  playgroundEventHandler(io, socket, rooms);
 
   socket.on('disconnect', () => {
     console.log('A user disconnected:', socket.id);
--- backend/src/utils/socket/playgroundEventHandler.js
+++ backend/src/utils/socket/playgroundEventHandler.js
@@ -1,14 +1,24 @@
-export const playgroundEventHandler = (io, socket) => {
+export const playgroundEventHandler = (io, socket, rooms) => {
   console.log('Playground event handler initialized for socket:', socket.id);
 
   // Join Room Event
   socket.on('join-room', ({ roomId }) => {
     console.log(`Socket ${socket.id} joining room: ${roomId}`);
     socket.join(roomId);
+    if (!rooms[roomId]) {
+      rooms[roomId] = {
+        code: ''
+      };
+    }
+
+    // Send existing code to the newly joined client
+    console.log(`Emitting init-code to socket ${socket.id} for room ${roomId}`);
+    socket.emit('init-code', { code: rooms[roomId].code });
   });
 
   // Code Change Event
   socket.on('code-change', ({ roomId, code }) => {
+    rooms[roomId].code = code;
     socket.to(roomId).emit('code-update', { code });
   });
 };

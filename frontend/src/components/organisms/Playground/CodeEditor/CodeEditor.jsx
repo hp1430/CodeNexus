8b97226aecb51bf2a99726f6a55b8e130f3a49f4
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-05-07 17:25:36 +0530

    feat: added basic awareness api

--- frontend/src/components/organisms/Playground/CodeEditor/CodeEditor.jsx
+++ frontend/src/components/organisms/Playground/CodeEditor/CodeEditor.jsx
@@ -1,14 +1,22 @@
-import { socket } from '@/configs/socketConfig';
+import useUserStore from '@/hooks/store/useUserStore';
 import { createYjsProvider } from '@/lib/yjs';
 import Editor from '@monaco-editor/react';
+import { useRef } from 'react';
 import { MonacoBinding } from 'y-monaco';
 
 const CodeEditor = ({ code, setCode, roomId, editorRef, monacoRef }) => {
+  const providerRef = useRef(null);
+  const awarenessRef = useRef(null);
+  const { user } = useUserStore();
+
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
     monacoRef.current = monaco;
 
     const { yText, provider } = createYjsProvider(roomId);
+
+    providerRef.current = provider;
+    awarenessRef.current = provider.awareness;
 
     new MonacoBinding(
       yText,
@@ -17,17 +25,38 @@ const CodeEditor = ({ code, setCode, roomId, editorRef, monacoRef }) => {
       provider.awareness
     );
 
-    //listen to cursor movement
-    editor.onDidChangeCursorPosition((e) => {
-      const position = e.position;
-      socket.emit('cursor-change', { roomId, position });
+    provider.awareness.setLocalStateField('user', {
+      id: user._id,
+      name: user.name,
+      color: '#ff0000',
     });
 
-    editor.onDidChangeCursorSelection((e) => {
+    provider.awareness.on('change', () => {
+      const states = provider.awareness.getStates();
+
+      states.forEach((state, clientId) => {
+        //skip current user
+        if (clientId === provider.doc.clientID) return;
+        console.log('Remote awareness state:', state);
+      });
+    });
+
+    //listen to cursor movement
+    editor.onDidChangeCursorPosition((e) => {
+      awarenessRef.current.setLocalStateField('cursor', {
+        lineNumber: e.position.lineNumber,
+        column: e.position.column,
+      });
+    });
+
+    editor.onDidChangeCursorPosition((e) => {
       const selection = e.selection;
-      socket.emit('selection-change', {
-        roomId,
-        selection,
+
+      awarenessRef.current.setLocalStateField('selection', {
+        startLineNumber: selection.startLineNumber,
+        startColumn: selection.startColumn,
+        endLineNumber: selection.endLineNumber,
+        endColumn: selection.endColumn,
       });
     });
   }
@@ -36,8 +65,6 @@ const CodeEditor = ({ code, setCode, roomId, editorRef, monacoRef }) => {
       height="100%"
       defaultLanguage="javascript"
       theme="vs-dark"
-      value={code}
-      onChange={(value) => setCode(value || '')}
       onMount={handleEditorDidMount}
       options={{
         fontSize: 14,

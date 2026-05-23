fe6639199a61f4c76678c5a1b12ea027cb8bf40b
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-05-23 18:14:00 +0530

    increased token expiry

--- frontend/src/pages/Homepage/HomepageWrapper.jsx
+++ frontend/src/pages/Homepage/HomepageWrapper.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HomePage from './Homepage';
+import { jwtDecode } from 'jwt-decode';
 import useUserStore from '@/hooks/store/useUserStore';
 
 export const HomepageWrapper = () => {
@@ -35,7 +36,29 @@ export const HomepageWrapper = () => {
     setOtpDialogOpen(value);
   }
 
+  function checkAuth() {
+    const token = useUserStore.getState().token;
+
+    if (!token) return;
+
+    try {
+      const decoded = jwtDecode(token);
+
+      const isExpired = decoded.exp * 1000 < Date.now();
+
+      if (isExpired) {
+        useUserStore.getState().logout();
+      }
+    } catch {
+      useUserStore.getState().logout();
+    }
+  }
+
   const isLoggedIn = !!user && !!token;
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
 
   return (
     <HomePage
